a1a15c73c3e6acb2b405c4bede7f0626039f8e0f
jakob <gatlingjakob@hotmail.com>
2017-09-26 13:00:57 +0200

    commit

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const path = __dirname + '/public/';
 
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.use(express.static('staticFiles'));
+
+// app.get('/', function (req, res) {
+//     res.send('<h1>Hello from root</h1>');
+// });
+
 app.get('/', function (req, res) {
-    res.send('<h1>Hello from root</h1>');
+    res.sendFile( path + 'keaEvents.html', function (err) {
+        //Her kan man handle errors
+    });
 });
 
 app.get('/testdigselv', function (req, res) {
@@ -24,7 +32,7 @@ app.post('/login', function (req, res) {
     // console.log(req.body.user);
     // console.log(req.body.password);
 
-    if (req.body.user === 'jaks'){
+    if (req.body.user === 'user'){
         res.sendFile( path + 'verysecret.html', function (err) {
             
         })
