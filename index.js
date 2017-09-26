d812f30a9cdfaa8dae57fb78d0bf7dd3763a9bf1
jakob <gatlingjakob@hotmail.com>
2017-09-26 13:13:31 +0200

    commit

--- index.js
+++ index.js
@@ -44,4 +44,6 @@ app.post('/login', function (req, res) {
 
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
